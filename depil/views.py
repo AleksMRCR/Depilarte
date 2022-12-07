bd13204e26e1a3d84be643aca25e011e5508bab3
AleksMRCR <106041615+AleksMRCR@users.noreply.github.com>
2022-12-07 18:19:08 -0300

    Funcionalidad de correo

--- depil/views.py
+++ depil/views.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from .models import *
 from .forms import *
 from django.template.loader import get_template
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives,EmailMessage
+
 
 
 # Create your views here.
@@ -22,24 +23,36 @@ def pedir_hora (request):
     if formulario.is_valid():
         formulario.save()
         mail = request.POST.get('email')
-        send_email(mail)
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        fecha = request.POST.get('fecha')
+        atencion = request.POST.get('id_prof')
+        comentario = request.POST.get('coment')
+        hora = request.POST.get('hora')
+        diab = request.POST.get('diab')
+        telefono = request.POST.get('telefono')
+        
+
+        email = EmailMessage('Web Depil-arte',
+        "{} {} ha solicitado una atencion con id {} el dia {} en el horario de {} con los siguientes comentarios:\n\n {} \n\n Paciente diabetico: {} \n Correo: {} \n Teléfono: {}".format(nombre,apellido,atencion,fecha,hora,comentario,diab,mail,telefono),
+        '',
+        ['contacto.depilarte1@gmail.com'],)
+        email.send()
+
+        aviso = EmailMessage('Web Depil-arte',
+        "{} {} su solicitud fue recibida satisfactoriamente, se le contactará a la brevedad para confirmar su hora".format(nombre,apellido),
+        '',
+        [mail],)
+        aviso.send()
+
+
         return redirect('hora')
     return render(request, 'paghoras/crear.html', {'formulario': formulario} ) 
+
+
 
 def hora (request):
     horas = Atencion.objects.all()
     return render(request, 'paghoras/index.html', {'horas': horas} )
 
-def send_email(mail):
-    context = {'mail':mail }
-    template = get_template('aviso.html')
-    content = template.render(context)
-    email = EmailMultiAlternatives(
-        'Un correo de prueba',
-        'de depilarte',
-        settings.EMAIL_HOST_USER,
-        [mail],
 
-    )
-    email.attach_alternative(content, 'text/html')
-    email.send()
